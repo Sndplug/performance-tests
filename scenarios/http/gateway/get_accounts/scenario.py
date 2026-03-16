0a4a1cd1124123a10adb7390efb14f63da9770f6
Sndplug <>
2026-03-16 09:45:32 +0500

    10.7 Работа с настройками Locust

---
+++ scenarios/http/gateway/get_accounts/scenario.py
@@ -0,0 +1,61 @@
+from locust import User, between, task
+from clients.http.gateway.accounts.schema import OpenDepositAccountResponseSchema, GetAccountsResponseSchema
+from clients.http.gateway.locust import GatewayHTTPTaskSet
+from clients.http.gateway.users.schema import CreateUserResponseSchema
+
+
+class GetAccountsTaskSet(GatewayHTTPTaskSet):
+    """
+    Нагрузочный сценарий, который последовательно:
+    1. Создаёт нового пользователя.
+    2. Открывает депозитный счёт.
+    3. Запрашивает список всех счетов для текущего пользователя.
+
+    Использует базовый GatewayHTTPTaskSet и уже созданных в нём API клиентов.
+    """
+
+    create_user_response: CreateUserResponseSchema | None = None
+    open_deposit_account_response: OpenDepositAccountResponseSchema | None = None
+    get_accounts_response: GetAccountsResponseSchema | None = None
+
+    @task(2)
+    def create_user(self):
+        """
+        Создаём нового пользователя и сохраняем результат для последующих шагов.
+        """
+        self.create_user_response = self.users_gateway_client.create_user()
+
+    @task(2)
+    def open_deposit_account(self):
+        """
+        Открываем депозитный для созданного пользователя.
+        Проверяем, что предыдущий шаг был успешным.
+        """
+        if not self.create_user_response:
+            return
+
+        self.open_deposit_account_response = self.accounts_gateway_client.open_deposit_account(
+            user_id=self.create_user_response.user.id
+        )
+
+    @task(6)
+    def get_accounts(self):
+        """
+        Запрашивает список всех счетов для текущего пользователя.
+        Проверяем, что предыдущий шаг был успешным.
+        """
+        if not self.create_user_response:
+            return
+
+        self.get_accounts_response = self.accounts_gateway_client.get_accounts(
+            user_id=self.create_user_response.user.id
+        )
+
+
+class GetDocumentsScenarioUser(User):
+    """
+    Пользователь Locust, исполняющий последовательный сценарий получения документов.
+    """
+    host = "localhost"
+    tasks = [GetAccountsTaskSet]
+    wait_time = between(1, 3)
